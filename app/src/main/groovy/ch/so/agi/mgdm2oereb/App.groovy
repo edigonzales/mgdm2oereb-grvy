bfd2399db0c4d3c43f70418ed7b1c0e96032845f
edigonzales <edi.gonzales@gmail.com>
2022-11-23 09:36:00 +0100

    initial commit

---
+++ app/src/main/groovy/ch/so/agi/mgdm2oereb/App.groovy
@@ -0,0 +1,11 @@
+package ch.so.agi.mgdm2oereb
+
+class App {
+    String getGreeting() {
+        return 'Hello World!'
+    }
+
+    static void main(String[] args) {
+        println new App().greeting
+    }
+}
